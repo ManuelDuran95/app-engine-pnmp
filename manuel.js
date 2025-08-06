4818a727d7c15f010309407e0e39df080290dea5
Manuel Durán <mata.duran@gmail.com>
2025-08-05 18:12:15 -0600

    Update manuel.js upate endoint

--- manuel.js
+++ manuel.js
@@ -26,7 +26,17 @@ app.get('/hello', (req, res) => {
    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
     res.send("This is your user jwt token: " + token)
 })
-
+app.get('/probador', (req, res) => {
+     const secretKey = 'example-signature-2srd';
+     const payload = {
+        userId: '123',
+        username: 'exampleUser2',
+        role: 'user'
+    };
+   const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+     const payload={error:"unable to make the request"};
+    res.status(500).send(payload)
+})
 const port = 8080
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
